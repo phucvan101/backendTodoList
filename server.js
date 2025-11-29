8c39bc21b8e2dfa48df467c304ae0c2a2d49fcf5
Phuc Nguyen <nguyenvanphuc10124@gmail.com>
2025-11-29 13:59:00 +0700

    setup and connect db

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+// import authRoutes from './routes/auth.routes.js';
+// import taskRoutes from './routes/task.routes.js';
+// import { errorHandler } from './middleware/error.middleware.js';
+const connectDB = require('./configs/db')
+
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to Database
+connectDB();
+
+// Routes
+// app.use('/api/auth', authRoutes);
+// app.use('/api/tasks', taskRoutes);
+
+// Health check
+app.get('/health', (req, res) => {
+    res.json({ status: 'OK', message: 'Server is running' });
+});
+
+
+
+// Error handling
+// app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
